bf1d578f1b3c4c241f1112f9792c68061012a5d6
juancyrus <mragbadinoahkekeli@gmail.com>
2024-05-01 08:53:44 +0000

    sol

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-""" importing Blueprint create flask app, app_views                        """                                                             
+""" index """                                                             
 from flask import jsonify
 from api.v1.views import app_views 
+from models import storage
 
 
-
-@app_views.route('/status')
+@app_views.route('/status',methods =["GET"],strict_slashes=False)
 def api_status():
     """
 
     """
-    response = {'status' : "OK"}
+    response = {"status" : "OK"}
     return jsonify(response)
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    """ Returns the number of each instance type """
+    return jsonify(amenities=storage.count("Amenity"),
+                   cities=storage.count("City"),
+                   places=storage.count("Place"),
+                   reviews=storage.count("Review"),
+                   states=storage.count("State"),
+                   users=storage.count("User"))
