e3498000a18dec38823b8c08bdaca6f9ef43fd9c
juancyrus <mragbadinoahkekeli@gmail.com>
2024-05-01 00:56:06 +0000

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,9 +13,9 @@ app.register_blueprint(app_views)
 
 
 
-if __name__ == '__main__':
-    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
-    PORT = int(getenv('HBNB_API_PORT',5000))
+if __name__ == "__main__":
+    host = getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(getenv('HBNB_API_PORT',5000))
     app.run(host=HOST,port=PORT, threaded=True)
 
  
